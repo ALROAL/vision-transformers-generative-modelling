81c050a01906b1d862a382aa10a763922b715c7c
Rasmus <r.juul97@gmail.com>
2022-03-06 18:06:13 +0100

    pytorch lightning integration and VAE implemented

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,22 +1,38 @@
-from src.data.load_dataset import make_dataloader
+import logging
+import torch
+from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+
+
+from src.models.models import VAE
+from src.data.make_dataset import dataset, MNISTDataModule
+from src import _PATH_DATA, _PATH_MODELS, _PROJECT_ROOT
 
 
 def main():
-    train_loader = make_dataloader(train=True,batch_size=64)
-    test_loader = make_dataloader(train=False,batch_size=64)
+    model = VAE()
+    mnist = MNISTDataModule()
+    mnist.setup()
+
+    checkpoint_callback = ModelCheckpoint(dirpath=_PATH_MODELS, monitor="val_loss", mode="min", save_top_k= 3)
+    early_stopping_callback = EarlyStopping(monitor="val_loss", patience=5, verbose=True, mode="min")
 
 
+    trainer = Trainer(max_epochs=200,
+                      gpus=1,
+                      precision=16,
+                      deterministic=True,
+                      default_root_dir=_PROJECT_ROOT,
+                      callbacks=[checkpoint_callback, early_stopping_callback],
+                      auto_lr_find=True)
+
+    trainer.tune(model, datamodule=mnist)
+    trainer.fit(model, datamodule=mnist)
 
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    seed_everything(42, workers=True)
 
     main()
