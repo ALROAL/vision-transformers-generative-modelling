9d1edd1ab68888f6fa42fb9f9f766aab5606f5d2
Rasmus <r.juul97@gmail.com>
2022-03-12 08:48:53 +0100

    bug fixing

--- submit.sh
+++ submit.sh
@@ -3,12 +3,10 @@
 #BSUB -J test
 #BSUB -n 4
 #BSUB -R "span[hosts=1]"
-#BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 00:15
-#BSUB -R "rusage[mem=32GB]"
-##BSUB -R "select[gpu32gb]"
-#BSUB -o ../outputs/gpu_%J.out
-#BSUB -e ../outputs/gpu_%J.err
+#BSUB -R "rusage[mem=16GB]"
+#BSUB -o outputs/gpu_%J.out
+#BSUB -e outputs/gpu_%J.err
 # -- end of LSF options --
 
 module load python3
@@ -16,5 +14,5 @@ source ../envs/aml/bin/activate
 
 # Name - Model - Epochs
 
-python3 main.py Classifier_test1 Classifier 2 >| ../outputs/class_test.out 2>| ../error/class_test.err
+python3 main.py Classifier_test1 Classifier 2 >| outputs/class_test.out 2>| error/class_test.err
 
