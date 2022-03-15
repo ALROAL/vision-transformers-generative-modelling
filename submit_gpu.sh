f1431d28ee3e7e19db1660ec5c08f2b90ae41c5a
Rasmus <r.juul97@gmail.com>
2022-03-15 09:19:27 +0100

    updated to cuda11 to use gpua100

---
+++ submit_gpu.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#BSUB -q gpua100
+#BSUB -J Classifier
+#BSUB -n 8
+#BSUB -R "span[hosts=1]"
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -W 01:00
+#BSUB -R "rusage[mem=8GB]"
+##BSUB -R "select[gpu32gb]"
+#BSUB -o outputs/gpu_%J.out
+#BSUB -e outputs/gpu_%J.err
+# -- end of LSF options --
+
+nvidia-smi
+
+source aml/bin/activate
+
+# Options
+# --name : Name for wandb
+# --model-type : Model type to train either ViTVAE or Classifier
+# --max-epochs : Number of max epochs
+# --num-workers : Number of threads use in loading data (should almost always be the same as BSUB -n)
+# --dim : Last dimension of output tensor after linear transformation
+# --depth : Number of Transformer blocks
+# --heads : Number of heads in Multi-head Attention layer
+# --mlp_dim : Dimension of the MLP (FeedForward) layer
+
+python3 main.py --name Classifier --model-type Classifier --max-epochs 200 --num-workers 8 --depth 16 >| outputs/class.out 2>| error/class.err
+
+# python3 main.py --name ViTVAE --model-type ViTVAE --max-epochs 200 --num-workers 8 >| outputs/ViTVAE.out 2>| error/ViTVAE.err
+
