39b45a9ba6112c6205a16437b7429040d9edda7c
Rasmus <r.juul97@gmail.com>
2022-04-02 16:45:18 +0200

    removing model from git

--- submit_v.sh
+++ submit_v.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #BSUB -q gpuv100
-#BSUB -J ViTVAE
+#BSUB -J ViTCVAE
 #BSUB -n 8
 #BSUB -R "span[hosts=1]"
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -W 01:00
+#BSUB -W 05:00
 #BSUB -R "rusage[mem=8GB]"
 ##BSUB -R "select[gpu32gb]" #options gpu16gb or gpu32gb
 #BSUB -o outputs/gpu_%J.out
@@ -15,17 +15,4 @@ nvidia-smi
 
 source aml/bin/activate
 
-# Options
-# --name : Name for wandb
-# --model-type : Model type to train either ViTVAE or Classifier
-# --max-epochs : Number of max epochs
-# --num-workers : Number of threads use in loading data (should almost always be the same as BSUB -n)
-# --dim : Last dimension of output tensor after linear transformation
-# --depth : Number of Transformer blocks
-# --heads : Number of heads in Multi-head Attention layer
-# --mlp_dim : Dimension of the MLP (FeedForward) layer
-
-# python3 main.py --name Classifier --model-type Classifier --max-epochs 200 --num-workers 8 >| outputs/class.out 2>| error/class.err
-
-python3 main.py --name ViTVAE --model-type ViTVAE --max-epochs 200 --num-workers 8 >| outputs/ViTVAE.out 2>| error/ViTVAE.err
-
+python3 main.py --name ViTCVAE_R --model-type ViTCVAE_R --dim 512 --mlp_dim 512 --batch_size 512 --depth 5 --ngf 32 --max-epochs 100 --num-workers 8 >| outputs/ViTCVAE_R.out 2>| error/ViTCVAE_R.err
