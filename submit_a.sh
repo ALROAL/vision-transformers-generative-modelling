ebd4894b743e2987fe41cfa157abdf381329da41
alroa <alroal98@gmail.com>
2022-04-04 11:24:10 +0200

    # submit_a.sh change

--- submit_a.sh
+++ submit_a.sh
@@ -25,4 +25,6 @@ source aml/bin/activate
 
 #python3 main.py --name ViTVAE --model-type ViTVAE --max-epochs 100 --num-workers 8 >| outputs/ViTVAE.out 2>| error/ViTVAE.err
 
-python3 main.py --name ConvCVAE --model-type ConvCVAE --dim 256 --batch_size 64 --max-epochs 100 --num-workers 8 >| outputs/ViTVAE.out 2>| error/ViTVAE.err
+#python3 main.py --name ConvCVAE --model-type ConvCVAE --dim 256 --batch_size 64 --max-epochs 100 --num-workers 8 >| outputs/ViTVAE.out 2>| error/ViTVAE.err
+
+python3 main.py --name ViTCVAE_R --model-type ViTCVAE_R --dim 256 --mlp_dim 256 --batch_size 64 --ngf 32 --max-epochs 100 --num-workers 8 >| outputs/ViTCVAE_R.out 2>| error/ViTCVAE_R.err
