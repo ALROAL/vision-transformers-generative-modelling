8369772b865ef3f16235a6f1db7cd5eeaec4890a
Sarah Miguel Cournane <s210291@student.dtu.dk>
2022-04-17 11:34:23 +0200

    patchGAN_DG2

--- submit_a.sh
+++ submit_a.sh
@@ -29,4 +29,4 @@ source aml/bin/activate
 
 #python3 main.py --name ViTCVAE_R --model-type ViTCVAE_R --dim 256 --mlp_dim 256 --batch_size 64 --ngf 32 --max-epochs 100 --num-workers 8 >| outputs/ViTCVAE_R.out 2>| error/ViTCVAE_R.err
 
-python3 main.py --name ViTVAE_PatchGAN_DG --model-type ViTVAE_PatchGAN --batch_size 8 --max-epochs 100 --num-workers 8 >| outputs/ViTVAE_PatchGAN.out 2>| error/ViTVAE_PatchGAN.err
+python3 main.py --name ViTVAE_PatchGAN_DG1 --model-type ViTVAE_PatchGAN --batch_size 8 --max-epochs 100 --num-workers 8 >| outputs/ViTVAE_PatchGAN1.out 2>| error/ViTVAE_PatchGAN1.err
