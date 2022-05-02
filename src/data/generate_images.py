f8a0528979b104b68b3b79f9c01e382280a7d7ab
Rasmus <r.juul97@gmail.com>
2022-05-02 13:38:43 +0200

    path fixes

--- src/data/generate_images.py
+++ src/data/generate_images.py
@@ -4,16 +4,16 @@ from src.models.models import CViTVAE, ConvCVAE
 
 
 if __name__ == "__main__":
-    model = CViTVAE.load_from_checkpoint("/work3/s164564/Vision-transformers-for-generative-modeling/models/CViTVAE2022-04-29-1735/CViTVAE-epoch=174.ckpt")
+    model = CViTVAE.load_from_checkpoint(_PATH_MODELS+"/CViTVAE2022-04-29-1735/CViTVAE-epoch=174.ckpt")
     model.eval()
     for i in range(10):
         img = model.forward_2(torch.zeros(1),1000)
         for j,im in enumerate(img):
-            save_image(im,"../data/CViTVAE/{}.png".format(1000*i+j))
+            save_image(im,_PATH_DATA+"/CViTVAE/{}.png".format(1000*i+j))
 
-    model = ConvCVAE().load_from_checkpoint("F:\Vision-transformers-for-generative-modeling\models\ConvCVAE2022-04-30-1854\ConvCVAE-epoch=349.ckpt")
+    model = ConvCVAE().load_from_checkpoint(_PATH_MODELS+"/ConvCVAE2022-04-30-1854/ConvCVAE-epoch=349.ckpt")
     model.eval()
     for i in range(10):
         img = model.forward_2(torch.zeros(1),1000)
         for j,im in enumerate(img):
-            save_image(im,"../data/CViTVAE/{}.png".format(1000*i+j))
+            save_image(im,_PATH_DATA+"/ConvCVAE/{}.png".format(1000*i+j))
