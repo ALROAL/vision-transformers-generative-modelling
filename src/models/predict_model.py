7f23b8e8dcbe2075f617227208ad3b451ad60d70
Rasmus <r.juul97@gmail.com>
2022-05-02 20:42:50 +0200

    testing

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -8,43 +8,64 @@ from src.data.make_dataset import CelebADataModule
 from src.models.models import  CViTVAE, ConvCVAE
 
 
-wandb_logger = WandbLogger(project="ViT-VAE", name="Testing")
+
 
 seed_everything(1234, workers=True)
 
-celeb = CelebADataModule(batch_size=1024, num_workers=0)
+celeb = CelebADataModule(batch_size=1024, num_workers=24)
 celeb.setup()
 
+wandb_logger = WandbLogger(project="ViT-VAE", name="CViTVAE2022-04-29-1423")
 trainer = Trainer(gpus=-1,logger=wandb_logger)
 
 model = CViTVAE.load_from_checkpoint(_PATH_MODELS+"/CViTVAE2022-04-29-1423/CViTVAE-epoch=170.ckpt")
 model.eval()
 trainer.test(model,datamodule=celeb)
 
+wandb_logger = WandbLogger(project="ViT-VAE", name="CViTVAE2022-04-29-1606")
+trainer = Trainer(gpus=-1,logger=wandb_logger)
+
 model = CViTVAE.load_from_checkpoint(_PATH_MODELS+"/CViTVAE2022-04-29-1606/CViTVAE-epoch=133.ckpt")
 model.eval()
 trainer.test(model,datamodule=celeb)
+
+wandb_logger = WandbLogger(project="ViT-VAE", name="CViTVAE2022-04-29-1735")
+trainer = Trainer(gpus=-1,logger=wandb_logger)
 
 model = CViTVAE.load_from_checkpoint(_PATH_MODELS+"/CViTVAE2022-04-29-1735/CViTVAE-epoch=174.ckpt")
 model.eval()
 trainer.test(model,datamodule=celeb)
 
+wandb_logger = WandbLogger(project="ViT-VAE", name="ConvCVAE2022-04-29-2156")
+trainer = Trainer(gpus=-1,logger=wandb_logger)
 
 model = ConvCVAE.load_from_checkpoint(_PATH_MODELS+"/ConvCVAE2022-04-29-2156/ConvCVAE-epoch=638.ckpt")
 model.eval()
 trainer.test(model,datamodule=celeb)
 
+wandb_logger = WandbLogger(project="ViT-VAE", name="ConvCVAE2022-04-30-0830")
+trainer = Trainer(gpus=-1,logger=wandb_logger)
+
 model = ConvCVAE.load_from_checkpoint(_PATH_MODELS+"/ConvCVAE2022-04-30-0830/ConvCVAE-epoch=619.ckpt")
 model.eval()
 trainer.test(model,datamodule=celeb)
+
+wandb_logger = WandbLogger(project="ViT-VAE", name="ConvCVAE2022-04-30-0830")
+trainer = Trainer(gpus=-1,logger=wandb_logger)
 
 model = ConvCVAE.load_from_checkpoint(_PATH_MODELS+"/ConvCVAE2022-04-30-0830/ConvCVAE-epoch=792.ckpt")
 model.eval()
 trainer.test(model,datamodule=celeb)
 
+wandb_logger = WandbLogger(project="ViT-VAE", name="ConvCVAE2022-04-30-1854")
+trainer = Trainer(gpus=-1,logger=wandb_logger)
+
 model = ConvCVAE.load_from_checkpoint(_PATH_MODELS+"/ConvCVAE2022-04-30-1854/ConvCVAE-epoch=349.ckpt")
 model.eval()
 trainer.test(model,datamodule=celeb)
+
+wandb_logger = WandbLogger(project="ViT-VAE", name="ConvCVAE2022-05-01-2202")
+trainer = Trainer(gpus=-1,logger=wandb_logger)
 
 model = ConvCVAE.load_from_checkpoint(_PATH_MODELS+"/ConvCVAE2022-05-01-2202/ConvCVAE-epoch=196.ckpt")
 model.eval()
